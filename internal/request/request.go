1131c0b68dbaa02115b07d22cfbdd3a1d0be843c
Arish Imam <arish.imam@gmail.com>
2025-03-29 15:59:02 -0700

    request refactor + additional tests

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,7 +1,10 @@
 package request
 
 import (
+	"bytes"
+	"fmt"
 	"io"
+	"strings"
 )
 
 type Request struct {
@@ -14,6 +17,74 @@ type RequestLine struct {
 	Method        string
 }
 
+const crlf = "\r\n"
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
+	rawBytes, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	requestLine, err := parseRequestLine(rawBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{
+		RequestLine: *requestLine,
+	}, nil
+}
+
+func parseRequestLine(data []byte) (*RequestLine, error) {
+	idx := bytes.Index(data, []byte(crlf))
+	if idx == -1 {
+		return nil, fmt.Errorf("could not find CRLF in request line\n")
+	}
+
+	requestLineText := string(data[:idx])
+
+	requestLine, err := requestLineFromString(requestLineText)
+	if err != nil {
+		return nil, err
+	}
+
+	return requestLine, nil
+}
+
+func requestLineFromString(request string) (*RequestLine, error) {
+	parts := strings.Split(request, " ")
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("poorly formatted request-line: %s\n", request)
+	}
+
+	method := parts[0]
+	for _, c := range method {
+		if c < 'A' || c > 'Z' {
+			return nil, fmt.Errorf("invalid method: %s\n", method)
+		}
+	}
+
+	target := parts[1]
+
+	versionParts := strings.Split(parts[2], "/")
+	if len(versionParts) != 2 {
+		return nil, fmt.Errorf("malformed request line: %s\n", request)
+	}
+
+	httpPart := versionParts[0]
+	if httpPart != "HTTP" {
+		return nil, fmt.Errorf("unrecognized http version: %s\n", httpPart)
+	}
+
+	version := versionParts[1]
+	if version != "1.1" {
+		return nil, fmt.Errorf("unrecognized http version: %s\n", httpPart)
+	}
+
+	return &RequestLine{
+		HttpVersion:   version,
+		RequestTarget: target,
+		Method:        method,
+	}, nil
 
 }
